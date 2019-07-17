0ff66db15951f99fc3aade011dff6ddd57673ed3
gerritt <gmocore@gmail.com>
2019-07-16 20:02:46 -0600

    finished js functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,16 +6,54 @@ let wins = 0;
 let losses = 0;
 let remainingGuesses = 9;
 let incorrectGuesses = []
+let letter;
+
+
+
+
+document.addEventListener('keyup', function (event) {
+    let userGuess = event.key
+    if (userGuess === letter) {
+        console.log('Correct')
+        wins++;
+        winsDisplay.textContent = wins
+        newGame()
+    } else {
+        incorrectGuesses.push(userGuess)
+        scoreCheck()
+    }
+
+})
 
 function randomLetter() {
     let letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    let letter;
     for (let i = 0; i < letterArray.length; i++) {
         let randomIndex = Math.floor(Math.random() * letterArray.length)
         letter = letterArray[randomIndex]
     }
     console.log(letter)
+    return letter
 }
 
 randomLetter()
+
+function scoreCheck() {
+    remainingGuesses--
+    remainingGuessesDisplay.textContent = remainingGuesses
+    incorrectGuessesDisplay.textContent = incorrectGuesses.join(' ').toUpperCase()
+    if (remainingGuesses < 1) {
+        losses++
+        lossesDisplay.textContent = losses
+        newGame()
+    }
+}
+
+function newGame() {
+    remainingGuesses = 9
+    incorrectGuesses = []
+    incorrectGuessesDisplay.textContent = ''
+    randomLetter()
+}
+
+
 
